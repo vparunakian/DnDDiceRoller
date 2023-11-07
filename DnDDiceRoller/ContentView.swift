11fd6ce63df4a57b795da3ef495ecee80384d12f
volodymyr.parunakian <volodymyr.parunakian@gmail.com>
2023-11-06 17:24:54 -0800

    create initial boilerplate project

---
+++ DnDDiceRoller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DnDDiceRoller
+//
+//  Created by Volodymyr Parunakian on 06.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DnDDiceRoller/DnDDiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDDiceRollerApp.swift
+//  DnDDiceRoller
+//
+//  Created by Volodymyr Parunakian on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DnDDiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
